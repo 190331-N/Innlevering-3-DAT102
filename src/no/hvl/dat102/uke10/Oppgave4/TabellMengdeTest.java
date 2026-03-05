b2fd0c02af0867d549dcf6eff6f45b99e40074d0
190331-N <190331@hvl.no>
2026-03-05 11:42:46 +0100

    dd

--- src/no/hvl/dat102/uke10/Oppgave4/TabellMengdeTest.java
+++ src/no/hvl/dat102/uke10/Oppgave4/TabellMengdeTest.java
@@ -12,25 +12,105 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TabellMengdeTest {
 
 
-    private MengdeADT<Integer> tom;
+    private MengdeADT<Integer> m;
     private MengdeADT<Integer> a;
     private MengdeADT<Integer> b;
+    private MengdeADT<Integer> c;
 
     @BeforeEach
     void setup() {
-        tom = new TabellMengde<>();
+
+        m = new TabellMengde<>();
         a = new TabellMengde<>();
         b = new TabellMengde<>();
+        c = new TabellMengde<>();
+
     }
 
-    private static <T> Set<T> asSet(T[] arr) {
-        return new HashSet<>(Arrays.asList(arr));
+    @Test
+    void nyMengdeErTom() {
+
+        assertTrue(m.erTom());
+        assertEquals(0, m.antallElementer());
+    }
+    @Test
+    void leggTilOgInneholder() {
+
+        m.leggTil("A");
+        m.leggTil("B");
+
+        assertFalse(m.erTom());
+        assertTrue(m.inneholder("A"));
+        assertTrue(m.inneholder("B"));
+        assertFalse(m.inneholder("c"));
+        assertEquals(2, m.antallElementer());
+    }
+    @Test
+    void leggTilSkalIkkeGiDuplikater() {
+
+        m.leggTil("A");
+        m.leggTil("A");
+        m.leggTil("A");
+
+        assertEquals(1, m.antallElementer());
+        assertTrue(m.inneholder("A"));
     }
 
-@Test
-    void nyMengdeSkalVereTom() {
+    @Test
+    void fjernSkalFjerne() {
 
-}
+        m.leggTil(1);
+        m.leggTil(2);
+        m.leggTil(3);
+        m.fjern(2);
 
+        assertFalse(m.inneholder(2));
+        assertEquals(2, m.antallElementer());
+    }
+    @Test
+    void unionSkalGiAlle() {
 
-}
+        a.leggTil(1);
+        a.leggTil(2);
+        b.leggTil(2);
+        b.leggTil(3);
+
+        MengdeADT<Integer> u = a.union(b);
+
+        assertTrue(u.inneholder(1));
+        assertTrue(u.inneholder(2));
+        assertTrue(u.inneholder(3));
+        assertEquals(3, u.antallElementer());
+    }
+    @Test
+    void snittSkalGiFelles() {
+
+        a.leggTil(1);
+        a.leggTil(2);
+        a.leggTil(3);
+        b.leggTil(2);
+        b.leggTil(4);
+
+        MengdeADT<Integer> s = a.snitt(b);
+
+        assertTrue(s.inneholder(2));
+        assertFalse(s.inneholder(1));
+        assertFalse(s.inneholder(3));
+        assertFalse(s.inneholder(4));
+        assertEquals(1,s.antallElementer());
+    }
+    @Test
+    void erLik_erDisjunkt_erDelMengde() {
+
+        a.leggTil(1);
+        a.leggTil(2);
+        b.leggTil(2);
+        b.leggTil(1);
+        c.leggTil(3);
+
+        assertTrue(a.erLik(b));
+        assertTrue(a.erDelmengdeAv(b));
+        assertTrue(a.erDisjunkt(c));
+        assertFalse(a.erDisjunkt(b));
+    }
+    }
