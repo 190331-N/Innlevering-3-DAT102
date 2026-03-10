892a9be803db26253feca9fdd5b58c8c77ebacab
HansArne04 <hansarnemid@gmail.com>
2026-03-10 13:36:29 +0100

    s

--- src/no/hvl/dat102/uke10/Oppgave4/IMPL/JavaSetToMengde.java
+++ src/no/hvl/dat102/uke10/Oppgave4/IMPL/JavaSetToMengde.java
@@ -38,51 +38,101 @@ public class JavaSetToMengde<T> implements MengdeADT<T> {
 
     @Override
     public boolean erLik(MengdeADT<T> annenMengde) {
-        return false;
+        if (set.size() != annenMengde.antallElementer()){
+            return false;
+        }
+        for (T element : set){
+            if (!annenMengde.inneholder(element)) {
+            return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean erDisjunkt(MengdeADT<T> annenMengde) {
-        return false;
+        for (T element : set){
+            if (annenMengde.inneholder(element)) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
     @Override
     public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+
+        for (T element : set){
+            if (annenMengde.inneholder(element)){
+                nyMengde.leggTil(element);
+            }
+        }
+        return nyMengde;
     }
 
     @Override
     public MengdeADT<T> union(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+
+        nyMengde.leggTilAlleFra(annenMengde);
+        for (T element : set){
+            if (!annenMengde.inneholder(element)){
+                nyMengde.leggTil(element);
+            }
+        }
+        return nyMengde;
     }
 
     @Override
     public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+
+        for (T element : set){
+            if (!annenMengde.inneholder(element)){
+                nyMengde.leggTil(element);
+            }
+        }
+        return nyMengde;
     }
 
     @Override
     public void leggTil(T element) {
+        set.add(element);
 
     }
 
     @Override
     public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+        T[] tab = (T[]) annenMengde.tilTabell();
 
+        for (int i = 0; i < tab.length; i++) {
+            set.add(tab[i]);
+        }
     }
 
     @Override
     public T fjern(T element) {
+        if (set.remove(element)) {
+            return element;
+        }
         return null;
     }
 
     @Override
     public T[] tilTabell() {
-        return null;
+        T[] tab = (T[]) new Object[set.size()];
+        int i = 0;
+        for (T element : set){
+            tab[i] = element;
+            i++;
+        }
+        return tab;
     }
 
     @Override
     public int antallElementer() {
-        return 0;
+        return set.size();
     }
 }
