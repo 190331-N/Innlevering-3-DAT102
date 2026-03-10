3f864ebe523aea1f11a2440c9474b1ae8480a7bc
HansArne04 <hansarnemid@gmail.com>
2026-03-10 12:48:59 +0100

    Update JavaSetToMengde.java

--- src/no/hvl/dat102/uke10/Oppgave4/IMPL/JavaSetToMengde.java
+++ src/no/hvl/dat102/uke10/Oppgave4/IMPL/JavaSetToMengde.java
@@ -2,21 +2,38 @@ package no.hvl.dat102.uke10.Oppgave4.IMPL;
 
 import no.hvl.dat102.uke10.Oppgave4.ADT.MengdeADT;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class JavaSetToMengde<T> implements MengdeADT<T> {
+
+    private Set<T> set;
+
+    public JavaSetToMengde(){
+        set = new HashSet<>();
+    }
 
     @Override
     public boolean erTom() {
-        return false;
+        return set.isEmpty();
     }
 
     @Override
     public boolean inneholder(T element) {
+        if (set.contains(element)){
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
-        return false;
+        for (T element : set){
+            if (!annenMengde.inneholder(element)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
