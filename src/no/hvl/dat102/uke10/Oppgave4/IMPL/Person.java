3db865f898e9f8b39f89d18ab6c8c920419bedc9
190331-N <190331@hvl.no>
2026-03-11 13:04:18 +0100

    sdf

---
+++ src/no/hvl/dat102/uke10/Oppgave4/IMPL/Person.java
@@ -0,0 +1,26 @@
+package no.hvl.dat102.uke10.Oppgave4.IMPL;
+
+import no.hvl.dat102.uke10.Oppgave4.ADT.MengdeADT;
+
+public class Person {
+
+    String navn;
+    private MengdeADT<String> hobbyer;
+
+    private Person(String navn, String... hobbyer) {
+        this.navn = navn;
+        this.hobbyer = new LenketMengde<>();
+
+        for (String h : hobbyer) {
+            this.hobbyer.leggTil(h);
+        }
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public MengdeADT<String> getHobbyer() {
+        return hobbyer;
+    }
+}
